01b6defe1d2a1db0612586bf1a785ef20228f43c
busheng <bulou@eng.ucsd.edu>
2016-03-03 17:10:33 +0800

    1

--- zhibo/run_all.sh
+++ zhibo/run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/bin/bash
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
-cd /root/soc/zhibo
-python /root/soc/zhibo/reset.py
+cd /root/spider/zhibo
+python /root/spider/zhibo/reset.py
 scrapy crawl panda
 scrapy crawl huya
 scrapy crawl douyu
